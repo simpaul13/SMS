91e8bb756a9c81a1ee73efb71e582dde522d6a88
simpaul13 <28johnpaulsim28@gmail.com>
2022-04-10 12:34:09 +0800

    Create Student :wrench:
    Create Database Student, Models Student, Policies Student And Controller Student

---
+++ database/migrations/2022_04_10_040443_create_students_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('student_number')->unique();
+            $table->string('student_firstname');
+            $table->string('student_lastname');
+            $table->string('student_username')->unique();
+            $table->string('student_email')->unique();
+            $table->string('student_password');
+            $table->string('student_phone');
+            $table->string('student_address');
+            $table->string('student_city');
+            $table->string('student_state');
+            $table->string('student_zip');
+            $table->string('student_country');
+            $table->enum('student_status', ['active', 'inactive']);
+            $table->enum('student_gender', ['male', 'female']);
+            $table->date('student_birthday');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
