d3a83121c63d261b543a6fafd947edb99dc346a4
John Paul Sim <johnpaul.sim@hmrphils.com>
2022-07-10 11:15:27 +0800

    Fix ERROR

--- resources/js/components/routes.js
+++ resources/js/components/routes.js
@@ -1,35 +1,5 @@
 export default {
     routes: [
-        {
-            path : "/",
-            component: require('./test/example').default,
-        },
-        // classroom
-        {
-            path : "/classroom",
-            component: require('./classroom/index').default,
-        },
-        // course
-        {
-            path : "/course",
-            component: require('./course/index').default,
-        },
-        // student
-        {
-            path : "/student",
-            component: require('./student/index').default,
-        },
-        // subject
-        {
-            path : "/subject",
-            component: require('./subject/index').default,
-        },
-        // teacher
-        {
-            path : "/teacher",
-            component: require('./teacher/index').default,
-        },
-
 
     ],
 
